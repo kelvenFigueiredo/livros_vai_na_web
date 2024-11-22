05a83d7a18132f7777326c5762baed3a15af4a39
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2024-11-22 21:32:03 +0200

    FEAT: create route file

---
+++ src/routes/AppRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from '../pages/Home/Home'
+import DonatedBooks from '../pages/DonatedBooks/DonatedBooks'
+import ErrorPage from '../pages/Error/ErrorPage'
+import Header from '../components/Header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Donate from '../pages/Donate/Donate'
+
+const AppRoute = () =>{
+  return(
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/DonatedBooks' element={<DonatedBooks/>}/>
+        <Route path='/Donate' element={<Donate/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default AppRoute
