afaf21b804b5dc5475150cfbb01e69d9a9a368d6
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2024-11-22 21:37:52 +0200

    FEAT: add header componentes and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import s from './header.module.scss';
+import logo1 from './../../assets/logo/logo1.png'
+import searchIcon from './../../assets/icons/search.png'
 
 function Header() {
+
   return (
-    <div>Header</div>
+    <header className={s.header}>
+      <section>
+        <Link className={s.logoHeader} to="/">
+          <img src={logo1} alt="logo" />
+          <h1>livros vai na web</h1>
+        </Link> 
+      </section>
+      <nav className={s.navHeader}>
+        <ul>
+          <li>
+            <Link className={s.link} to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link className={s.link} to="/DonatedBooks">Livros Doados</Link>
+          </li>
+          <li>
+            <Link className={s.link} to="/Donate">Quero Doar</Link>
+          </li>
+          <li></li>
+        </ul>
+      </nav>
+      <section className={s.searchBar}>
+        <input
+          type="search" 
+          name=""
+          id=""
+          placeholder="O que você procura?"
+        />
+        <button>
+          <img 
+            src={searchIcon} 
+            alt="Icone de lupa" 
+          />
+        </button>
+      </section>
+    </header>
   )
 }
 
