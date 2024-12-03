c0237ef91c355143f7c4dff4d52b85fd33e9e91a
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2024-12-03 13:42:36 +0200

    FEAT: add donatebook pages and style

--- src/pages/DonatedBooks/DonatedBooks.jsx
+++ src/pages/DonatedBooks/DonatedBooks.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import s from './donateBooks.module.scss';
+import BookCard from '../../components/Cards/BookCards/BookCard';
+import Book from './../../assets/imgs/book.png';
 
 const DonatedBooks = () => {
   return (
-    <center>
-        <h1>DonateBooks</h1>
-    </center>
+    <>
+      <section className={s.container}>
+        <div className={s.headerContent}>
+          <h1>livros doados</h1>
+        </div>
+        <div className={s.content}>
+          <BookCard imagem={Book} imgDescription= "Imagem de livro " Text="O protagonista Susanne Andrade Ficção" />
+        </div>
+      </section>
+    </>
   )
 }
 
