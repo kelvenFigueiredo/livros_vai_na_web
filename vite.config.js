d20ca19ba49b7ce1a22836f30cbabbd427b2ed7b
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2024-12-09 16:59:46 +0200

    FIX: deploy problem

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/livros_vai_na_web/', // Nome do repositório no GitHub
   plugins: [react()],
 })
